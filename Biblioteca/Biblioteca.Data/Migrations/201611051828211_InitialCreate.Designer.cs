caca6d7d52eb274da60b896feb3b9d788548fd03
Daniel Rodriguez <daniel_12_1994@yahoo.com>
2016-11-05 15:04:16 -0600

    Agregando procesos

--- Biblioteca/Biblioteca.Data/Migrations/201611051828211_InitialCreate.Designer.cs
+++ Biblioteca/Biblioteca.Data/Migrations/201611051828211_InitialCreate.Designer.cs
@@ -7,13 +7,13 @@ namespace Biblioteca.Data.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class agregarEditorial : IMigrationMetadata
+    public sealed partial class InitialCreate : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(agregarEditorial));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(InitialCreate));
         
         string IMigrationMetadata.Id
         {
-            get { return "201610292057187_agregarEditorial"; }
+            get { return "201611051828211_InitialCreate"; }
         }
         
         string IMigrationMetadata.Source
--- Biblioteca/Biblioteca.Data/Migrations/201611051828211_InitialCreate.cs
+++ Biblioteca/Biblioteca.Data/Migrations/201611051828211_InitialCreate.cs
@@ -3,7 +3,7 @@ namespace Biblioteca.Data.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class agregarEditorial : DbMigration
+    public partial class InitialCreate : DbMigration
     {
         public override void Up()
         {
@@ -16,16 +16,26 @@ namespace Biblioteca.Data.Migrations
                     })
                 .PrimaryKey(t => t.Id);
             
-            AddColumn("dbo.Libroes", "Editorial_Id", c => c.Int());
-            CreateIndex("dbo.Libroes", "Editorial_Id");
-            AddForeignKey("dbo.Libroes", "Editorial_Id", "dbo.Editorials", "Id");
+            CreateTable(
+                "dbo.Libroes",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Nombre = c.String(),
+                        Año = c.Int(nullable: false),
+                        Editorial_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Editorials", t => t.Editorial_Id)
+                .Index(t => t.Editorial_Id);
+            
         }
         
         public override void Down()
         {
             DropForeignKey("dbo.Libroes", "Editorial_Id", "dbo.Editorials");
             DropIndex("dbo.Libroes", new[] { "Editorial_Id" });
-            DropColumn("dbo.Libroes", "Editorial_Id");
+            DropTable("dbo.Libroes");
             DropTable("dbo.Editorials");
         }
     }
