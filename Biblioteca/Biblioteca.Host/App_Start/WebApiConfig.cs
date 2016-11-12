3fe4b4cd83e87ac60101d7c4ad4ac30047696357
Daniel Rodriguez <daniel_12_1994@yahoo.com>
2016-11-12 14:58:21 -0600

    Mas Controllers

--- Biblioteca/Biblioteca.Host/App_Start/WebApiConfig.cs
+++ Biblioteca/Biblioteca.Host/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using Newtonsoft.Json;
 
 namespace Biblioteca.Host
 {
@@ -9,6 +10,8 @@ namespace Biblioteca.Host
     {
         public static void Register(HttpConfiguration config)
         {
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore; //esto fue ingresado
+            
             // Web API configuration and services
 
             // Web API routes
