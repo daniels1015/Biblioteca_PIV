1ebfd679b8218d26338ac9ff9c05caca88e5ef07
Daniel Rodriguez <daniel_12_1994@yahoo.com>
2016-11-19 14:56:41 -0600

    Agregando controladores html

--- Biblioteca/Biblioteca.Host/App_Start/BundleConfig.cs
+++ Biblioteca/Biblioteca.Host/App_Start/BundleConfig.cs
@@ -22,6 +22,12 @@ namespace Biblioteca.Host
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include("~/Scripts/angular.js", 
+                "~/Scripts/angular-route.js", 
+                "~/Scripts/app/app.js",
+                "~/Scripts/app/routes.js",
+                "~/Scripts/app/home/home.controller.js")); //EL ~/Scripts/angular.js siempre va primero y luego todo lo que tenemos que agregar.
         }
     }
 }
---
+++ Biblioteca/Biblioteca.Host/Scripts/app/routes.js
@@ -0,0 +1,13 @@
+﻿app.config(['$routeProvider',
+    function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: "/Scripts/app/home/home.template.html",
+                controller: "homeController"
+                //template: "<p>Hola Mundo con Angular.</p>"
+            })
+            .otherwise({
+                   redirectTo: '/'
+                })
+    }
+]);
