541107589ce9236e32e55cc76c19fe6b048638af
Daniel Rodriguez <daniel_12_1994@yahoo.com>
2016-12-03 14:57:32 -0600

    Haciendo frontend de Editorial

---
+++ Biblioteca/Biblioteca.Host/Scripts/app/editorial/editorial.service.js
@@ -0,0 +1,29 @@
+﻿app.service('editorialService',
+    [
+        '$http',
+        'miConfiguracion',
+        function ($http, miConfiguracion) {
+            function obtenerEditoriales() {
+                return $http.get(miConfiguracion.urlBackend + 'Api/Editorial');
+            }
+            
+            function agregarEditorial(nuevaEditorial) {
+                return $http.post('http://localhost:50295/Api/Editorial', nuevaEditorial);
+            }
+
+            function editarEditorial(editorial) {
+                return $http.put(miConfiguracion.urlBackend + 'Api/Editorial/' + editorial.Id, editorial);
+            }
+
+            function eliminarEditorial(editorial) {
+                return $http.delete(miConfiguracion.urlBackend + 'Api/Editorial/' + editorial.Id);
+            }
+
+            return {
+                editarEditorial: editarEditorial,
+                obtenerEditoriales: obtenerEditoriales,
+                agregarEditorial: agregarEditorial,
+                eliminarEditorial: eliminarEditorial
+            }
+        }
+    ]);
